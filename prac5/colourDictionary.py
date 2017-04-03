44475e5600d4e9c0bf96ace51c67e9c062a2c1bd
Jared <jared.cooke@my.jcu.edu.au>
2017-04-03 11:04:15 +1000

    fixed everything from code review

--- prac5/colourDictionary.py
+++ prac5/colourDictionary.py
@@ -5,7 +5,7 @@ print("These are the available colours:\n{}".format(COLOURS.keys()))
 colour = input("Please enter a colour: ")
 while not colour.strip():
     colour = input("Please enter a colour from list: ")
-found_code= False
+found_code = False
 while not found_code:
     if colour in COLOURS:
         print("The hexadecimal code for {} is {}".format(colour, COLOURS[colour.lower()]))
